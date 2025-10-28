a4908e9ae29cad7247dec93280d7238f4bc04d63
earn05869 <earn05869@gmail.com>
2025-10-28 23:46:09 +0700

    all map

---
+++ asset/hall/hall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="hall" tilewidth="64" tileheight="64" tilecount="66" columns="6">
+ <image source="hall.png" width="385" height="704"/>
+</tileset>
---
+++ asset/red/red.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="red" tilewidth="64" tileheight="64" tilecount="42" columns="6">
+ <image source="red.png" width="409" height="466"/>
+</tileset>
