857aca112fef386c481a201466f4a3dbf55e4166
phai-zk <chisanupong.m34.1@gmail.com>
2025-10-31 10:41:12 +0700

    a

--- game.py
+++ game.py
@@ -642,9 +642,9 @@ class Game:
 			alpha_value = 240  # user-tuned darkness
 			self.shion_dark_surface.fill((0, 0, 0, alpha_value))
 			# Player is rendered centered on screen in this project; cut a hole there
-			player_screen_center = (GameConfig.SCREEN_WIDTH // 2, GameConfig.SCREEN_HEIGHT // 2)
+			player_screen_center = (GameConfig.SCREEN_WIDTH // 2, GameConfig.SCREEN_HEIGHT // 2 - 20)
 			spot_radius = 80
-			pygame.draw.circle(self.shion_dark_surface, (0, 0, 0, 0), player_screen_center - 20, spot_radius)
+			pygame.draw.circle(self.shion_dark_surface, (0, 0, 0, 0), player_screen_center, spot_radius)
 			self.screen.blit(self.shion_dark_surface, (0, 0))
 		
 		# ===== 4. DRAW FADE OVERLAY =====
